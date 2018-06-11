1a64ca85bc6115300c7b523dc92b053e1699f144
satishburugu <satish.burugu@bhge.com>
2018-06-11 11:15:41 -0500

    updated the config fiels with eslint rules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import PropTypes from 'prop-types';
 
 import './style.scss';
 
-
-const title = "My new react project from scarcth "
+const title = 'My new react project fromdasd scratch ';
 
 ReactDOM.render(
-    <div>{title}</div>,
-    document.getElementById('app')
+	<div>
+		{title}
+
+		<button type="button" className="btn btn-danger">
+			Hello
+		</button>
+	</div>,
+
+	document.getElementById('app'),
 );
 
 module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,54 @@
-
 const webpack = require('webpack');
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    // we want to use index.js file as entry point to bundle all of its imported fifle 
-    entry: [ 
-        'react-hot-loader/patch',// this will help to apply the changes in the browser without reloading the browser
-       './src/index.js'
-    ],
-    //here we are adding dome loaders which will be responsible to bundle our source files
-    module: {
-        rules:[
-            {
-                test:/\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test:/\.scss$/,
-                use:[{
-                    loader: 'style-loader'
-                },{
-                    loader: 'css-loader'
-                },{
-                    loader: 'sass-loader'
-                }]
-            }
-        ]
-    },
-    resolve:{
-        extensions: ['*','.js','.jsx']
-    },
-    // here we are telling our webpack to output our bundle.js
-    output:{ 
-        path:path.resolve(__dirname + 'dist'),
-       // publicPath:'/',
-        filename: 'bundle.js'
-    },
-    plugins:[
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    // configuration will be providing the options for our dev server
-    devServer: {
-        contentBase: './dist',
-        hot:true
-    }
-}
+	// we want to use index.js file as entry point to bundle all of its imported fifle
+	entry: [
+		'react-hot-loader/patch', // this will help to apply the changes in the browser without reloading the browser
+		'./src/index.js'
+	],
+	//here we are adding dome loaders which will be responsible to bundle our source files
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			},
+			{
+				test: /\.js/,
+				exclude: /node_modules/,
+				use: ['babel-loader', 'eslint-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+	// here we are telling our webpack to output our bundle.js
+	output: {
+		path: path.resolve(__dirname + '/dist'),
+		publicPath: '/public/',
+		filename: 'bundle.js'
+	},
+	plugins: [new webpack.HotModuleReplacementPlugin()],
+	// configuration will be providing the options for our dev server
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	}
+};
